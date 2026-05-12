bee17967019f913499728eb70bf4e7b2fd38365c
chennakesavuulkummari <chennakesavulu.kummari@gmail.com>
2026-05-12 09:53:11 +0530

    plan-1

--- iac/preprod/provider.tf
+++ iac/preprod/provider.tf
@@ -6,7 +6,7 @@ terraform {
       version = "~> 5.0"
     }
   }
-
+}
 #   backend "s3" {
 #     bucket         = "c3ops-terraform-statefiles"
 #     key            = "c3_core_infra_preprod/terraform.tfstate"
